706a04f695be31e37cd9cbaf185e1e657b0be8d1
akbiggs <alex.biggs@utoronto.ca>
2015-03-31 00:50:29 -0400

    import code from A2 to use as base for game

---
+++ game/main.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.h
+ * Author: g1biggse
+ *
+ * Created on March 31, 2015, 12:48 AM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+class main {
+public:
+    main();
+    main(const main& orig);
+    virtual ~main();
+private:
+
+};
+
+#endif	/* MAIN_H */
+
