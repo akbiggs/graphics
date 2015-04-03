5a25f10ea802f0ab50d1ce7d7306fbfffb0d1752
SubjectFrost <breu.lorenz@gmail.com>
2015-04-03 17:35:22 -0400

    ihatethis.png.png.png

--- scene_object.cpp
+++ scene_object.cpp
@@ -150,7 +150,7 @@ bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
                         }
 						
                         ray.intersection.point = modelToWorld * (a + lam * d);
-						Vector3d normal = ray.intersection.point;
+						Vector3D normal = ray.intersection.point;
 						normal.normalize();
 						
                         ray.intersection.normal = transNorm(worldToModel, normal);
